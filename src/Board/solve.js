32e3e3eccdd0ca81c1f1aaba0607d6e54681a7ff
Alan Tay <lun.tay@gmail.com>
2021-09-04 22:14:06 +0800

    added comments

--- src/Board/solve.js
+++ src/Board/solve.js
@@ -5,22 +5,29 @@ export default function solve(boardState) {
     console.log("got it solution", boardState)
     return boardState;
   }
-  const tempState = JSON.parse(JSON.stringify(boardState));
+  const workingBoardState = JSON.parse(JSON.stringify(boardState));
   for (let rowIdx = 0; rowIdx < 9; rowIdx++) {
     for (let colIdx = 0; colIdx < 9; colIdx++) {
-      const cell = tempState[rowIdx][colIdx];
+      const cell = workingBoardState[rowIdx][colIdx];
+      // if cell is empty
       if (cell.trim() === "") {
+        // loop thru numbers 1 to 9
         for (let input = 1; input <= 9; input++) {
-          if (canInsert(input.toString(), rowIdx, colIdx, tempState)) {
-            tempState[rowIdx][colIdx] = input.toString();
-            const solution = solve(tempState);
+          // check if can insert
+          if (canInsert(input.toString(), rowIdx, colIdx, workingBoardState)) {
+            // check insert number
+            workingBoardState[rowIdx][colIdx] = input.toString();
+            // solve with new inserted number
+            const solution = solve(workingBoardState);
+            // check if board is filled with new solution
             if (checkFullBoard(solution)) {
               return solution;
             }
           }
-        }
-        return tempState;
-      }
+        }// end: loop thru numbers 1 to 9
+        // reaching here means no number can be inserted or number inserted did not give final solution
+        return workingBoardState;
+      }// end: if cell is empty
     }
   }
 }
